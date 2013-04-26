7e0fb75685cc7098006e3c72dfdef839a930f038
chajr <chajr@bluetree.pl>
2013-04-26 15:27:27 +0200

    Build main file structure (issue #1)
    This version introduces multiple enhancements:
    
    Added based file structure, with some important files (css, js)
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:   /index.php
                -- Added --
                    base page file with basic html structure
    
    Added:   /lib/Form/Form.php
                -- Added --
                    bses library file
    
    Added:   /js/jquery-1.8.2.min.js
                -- Added --
                    jQuery library
    
    Added:   /js/jquery-1.8.2.min.js
                -- Added --
                    jQuery UI library
    
    Added:   /css/960_24_col.css
                -- Added --
                    base grid style
    
    Added:   /css/meyer_reset_css.css
                -- Added --
                    reset style
    
    Added:   /definitions/basic.xml
                -- Added --
                    basic form definition
    
    Added:   /doc/documentation.php
                -- Added --
                    base file for documentation page
    
    Added:   /doc/bugs.php
                -- Added --
                    base file for bugs page
    
    Added:   /doc/examples.php
                -- Added --
                   base file for examples page
    
    Added:   /doc/header.php
                -- Added --
                    common page header file
    
    Added:   /doc/footer.php
                -- Added --
                    common page footer file
    
    Added:   /doc/todo.php
                -- Added --
                    base file for to do page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>XML Form Generator</title>
+        <link href="css/meyer_reset_css.css" rel="stylesheet" type="text/css" />
+        <link href="css/960_24_col.css" rel="stylesheet" type="text/css" />
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    
+    </body>
+</html>
